e0965ee613702145ad55b096f83088506a735520
Esdras24 <edras2424@gmail.com>
2024-08-18 23:24:56 -0600

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,3 +3,9 @@ export const environment = {
   //url: 'http://tecnosystem.me',
   url: 'https://tiemposbingoregalado.com',
 };
+
+export const bingoCredentials = {
+  url: 'https://bingoregalado.com/api-bingo-regalado/api',
+  email: 'sistematiempos-apiclient@bingoregalado.com',
+  password: 'g*H3$Olepr9!'
+}
